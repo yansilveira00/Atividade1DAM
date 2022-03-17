f0363518185951bc4a1fcbe1c277e76e38b597d0
Yan Silveira <89039535+yansilveira00@users.noreply.github.com>
2022-03-16 22:48:05 -0300

    Add files via upload

---
+++ src/screens/Home/styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Platform } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#000000',
+    flex: 1
+  },
+  text: {
+        fontSize: 30,
+        color: 'white',
+        textAlign: 'center',
+        justifyText: 'center',
+        backgroundColor: 'black',
+        margin: 20
+    }
+})
